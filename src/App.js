e452f31210867204b7078d23d4a339e5d5737f1e
Blake <bpro0821@gmail.com>
2019-08-23 08:53:54 -0500

    added footer instructions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Tetris from './components/Tetris'
+import './App.css'
 
 const App = () => (
   <div className = "App">
     <Tetris />
+    <p className = "finstructions">
+      Press Start Game to begin a new game. Use arrow keys "&#8593;" to rotate Tetromino, "&#8592;"  +  "&#8594;" arrows to move from left to right, and &#8595; to toggle the falling speed. 
+    </p>
   </div>
 )
 
